defa69900112d3d76e9ded5916050126be62fe5c
Patryk Pilek <patryk.pilek@gmail.com>
2016-09-03 18:41:51 +0100

    using if statemenrs in c++

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -10,8 +10,8 @@ void FBullCowGame::Reset()
 	constexpr int32 MAX_TRIES = 8;
 	MyMaxTries = MAX_TRIES;
 
-	const FString HIDDEN_WORD = "planet";
-	MyHiddenWord = HIDDEN_WORD;
+	const FString HIIDEN_WORD = "and";
+	MyHiidenWord = HIIDEN_WORD;
 
 	MyCurrentTry = 1;
 	return;
@@ -27,13 +27,33 @@ bool FBullCowGame::CheckGuessValidity(FString)
 	return false;
 }
 // receives a VALID guess, incriments turn, and returns count
-BullCowCount FBullCowGame::SubmitGuess(FString)
+FBullCowCount FBullCowGame::SubmitGuess(FString Guess)
 {
 	// incriment the turn number
 	MyCurrentTry++;
 	// setup a return variable
-	BullCowCount BullCowCount;
+	FBullCowCount BullCowCount;
 	// loop through all letters in the guess
+	int32 HiddenWordLenght = MyHiidenWord.length();
+	for (int32 i = 0; i < HiddenWordLenght; i++)
+	{
 		// copare letter against the hidden word
+		for (int32 j = 0; j < HiddenWordLenght; j++)
+		{
+			// if they match then
+			if (Guess[i] == MyHiidenWord[i])
+			{
+				// if they're in the same place
+				if (i == j)
+				{
+					BullCowCount.Bulls++; // incriment bulls
+				}
+				else
+				{
+					BullCowCount.Cows++; // must be a cow
+				}
+			}		
+		}	
+	}
 	return BullCowCount;
 }
--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -53,8 +53,11 @@ void PlayGame()
 	{
 		FText Guess = GetGuess(); // TODO make loop checking valid
 
-		// submit valid guess to the game
+		// submit valid guess to the game, and receive counts
+		FBullCowCount BullCowCount = BCGame.SubmitGuess(Guess);
 		// print number of bulls and cows
+		std::cout << "Bulls = " << BullCowCount.Bulls;
+		std::cout << " Cows = " << BullCowCount.Cows << std::endl;
 
 		std::cout << "Your guess was: " << Guess << std::endl;
 		std::cout << std::endl;
