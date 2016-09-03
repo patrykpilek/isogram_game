98b3869f692b99abf90f293d017934ef02f4d730
Patryk Pilek <patryk.pilek@gmail.com>
2016-09-03 15:58:26 +0100

    instantiating class

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
-
-using namespace std;
+#include "FBullCowGame.h"
 
 void PrintIntro();
 void PlayGame();
 bool AskToPlayAgain();
-string GetGuess();
+std::string GetGuess();
 
+FBullCowGame BCGame; // instantiate a new game
 
 // the entry point for our application
 int main()
@@ -26,37 +26,39 @@ int main()
 void PrintIntro()
 {
 	constexpr int WORLD_LENGHT = 5;
-	cout << "Welcome to Bulls and Cows, a fun word game.\n";
-	cout << "Can you guess the " << WORLD_LENGHT << " letter isogram I'm thinking off?\n";
-	cout << endl;
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << "Can you guess the " << WORLD_LENGHT << " letter isogram I'm thinking off?\n";
+	std::cout << std::endl;
 	return;
 }
 
 // loop for the number of turns asking for guesses
 void PlayGame()
 {
-	constexpr int NUMBER_OF_TUNRS = 5;
-	for (int i = 1; i <= NUMBER_OF_TUNRS; i++)
+	int MaxTries = BCGame.GetMaxTries();
+
+	for (int i = 1; i <= MaxTries; i++)
 	{
-		string Guess = GetGuess();
-		cout << "Your guess was: " << Guess << endl;
-		cout << endl;
+		std::string Guess = GetGuess();
+		std::cout << "Your guess was: " << Guess << std::endl;
+		std::cout << std::endl;
 	}
 }
 
 // get a guess from the player and repeat the guess back to them
-string GetGuess()
+std::string GetGuess()
 {
-	string Guess = "";
-	cout << "Enter your guess: ";
-	getline(cin, Guess);
+	std::string Guess = "";
+	int CurrentTry = BCGame.GetCurrentTry();
+	std::cout << "Try " << CurrentTry << ". Enter your guess: ";
+	std::getline(std::cin, Guess);
 	return Guess;
 }
 
 bool AskToPlayAgain()
 {
-	cout << "Do you want to play again (y/n)? ";
-	string Response = "";
-	getline(cin, Response);
+	std::cout << "Do you want to play again (y/n)? ";
+	std::string Response = "";
+	std::getline(std::cin, Response);
 	return (Response[0] == 'y' || Response[0] == 'Y');
 }
