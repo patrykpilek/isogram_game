7d8030b1e6903532d0a4ca7ae4d8eb45d585c28c
Patryk Pilek <patryk.pilek@gmail.com>
2016-09-03 17:37:22 +0100

    using struct for Simple Types

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -10,6 +10,9 @@ void FBullCowGame::Reset()
 	constexpr int32 MAX_TRIES = 8;
 	MyMaxTries = MAX_TRIES;
 
+	const FString HIDDEN_WORD = "planet";
+	MyHiddenWord = HIDDEN_WORD;
+
 	MyCurrentTry = 1;
 	return;
 }
@@ -22,4 +25,15 @@ bool FBullCowGame::isGameWon() const
 bool FBullCowGame::CheckGuessValidity(FString)
 {
 	return false;
+}
+// receives a VALID guess, incriments turn, and returns count
+BullCowCount FBullCowGame::SubmitGuess(FString)
+{
+	// incriment the turn number
+	MyCurrentTry++;
+	// setup a return variable
+	BullCowCount BullCowCount;
+	// loop through all letters in the guess
+		// copare letter against the hidden word
+	return BullCowCount;
 }
