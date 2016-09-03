ced383578193739097f8ab5de5f43d3d25d27fa5
Patryk Pilek <patryk.pilek@gmail.com>
2016-09-03 01:31:50 +0100

    magic numbers and constants

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World" << endl;
+	constexpr int WORLD_LENGHT = 5;
+
+	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Can you guess the " << WORLD_LENGHT;
+	cout << " letter isogram I'm thinking off?" << endl;
+
 	return 0;
 }
